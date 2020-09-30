790ef94c387f3179127f239e95249298ca1f21e9
gitgeoman <gitgeoman@users.noreply.github.com>
2020-09-30 15:06:45 +0200

    remove button update

--- src/App.js
+++ src/App.js
@@ -28,6 +28,38 @@ class App extends Component {
       this.setState({newTodo: event.target.value})
   }
 
+  toggleTodoDone(event, index){
+    console.log(event.target.checked);
+    const todos = [...this.state.todos]; //copy the array
+    todos[index] = {...todos[index]}; // copy the todo
+    todos[index].done = event.target.checked; // update done property on copied
+    console.log(todos);
+    this.setState({
+      todos
+    });
+  }
+
+  removeTodo(index){
+    const todos = [...this.state.todos];
+    todos.splice(index, 1);
+    this.setState({
+      todos
+    });
+  }
+
+
+  allDone(){
+    const todos = this.state.todos.map(todo=>{
+      return{
+        title: todo.title,
+        done:true
+      }
+    });
+    this.setState({
+      todos
+    });
+  }
+
   render(){
   return (
     <div className="App">
@@ -38,9 +70,26 @@ class App extends Component {
           <input onChange={(event)=>this.newTodoChange(event)} id="newTodo" name ="newTodo" value={this.state.newTodo}/>
             <button type="Submit">Add Todo</button>
       </form>
+      <button onClick={()=>this.allDone()}>AllDone</button>
+
+
       <ul>
-        {this.state.todos.map( todo=>{
-          return <li key={todo.title}>{todo.title}</li>
+        {this.state.todos.map((todo, index )=>{
+          return (
+            <li key={todo.title}>
+              <input 
+                onChange={(event)=>this.toggleTodoDone(event, index)} 
+                type="checkbox"
+                checked={todo.done}
+              />
+              {/*<span style={{textDecoration: todo.done?'line-through':'inherit'}}>{todo.title}
+              </span>
+            */}
+            <span className={todo.done?'done':''}>{todo.title}</span> 
+            <button onClick={()=>this.removeTodo(index)}>Remove</button>
+
+            </li>
+            )
         })}
       </ul>
 
