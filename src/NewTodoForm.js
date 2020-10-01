e806ce6b11adbdda7b7022311fd1343a17ecfcd6
gitgeoman <gitgeoman@users.noreply.github.com>
2020-10-01 11:45:30 +0200

    css

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -3,9 +3,9 @@ import React from 'react';
 const NewTodoForm = (props)=>{
 	return(
       <form onSubmit={props.formSubmitted}>
-          <label htmlFor="newTodo">New Todo</label>
+          <label htmlFor="newTodo">Add new Todo</label>
           <input onChange={props.newTodoChanged} id="newTodo" name ="newTodo" value={props.newTodo}/>
-            <button type="Submit">Add Todo</button>
+            <button type="Submit">Submit</button>
       </form>
 	)
 }
